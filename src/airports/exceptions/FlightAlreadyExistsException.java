c5b9910d840d595b0314bb9b74df51c16f3da28d
OscarPujol15 <opujolr@edu.tecnocampus.cat>
2025-04-07 18:40:17 +0200

    Update FlightAlreadyExistsException.java

--- src/airports/exceptions/FlightAlreadyExistsException.java
+++ src/airports/exceptions/FlightAlreadyExistsException.java
@@ -1,5 +1,8 @@
 package airports.exceptions;
 
 public class FlightAlreadyExistsException extends RuntimeException{
+  public FlightAlreadyExistsException (String msg) {
+        super(msg);
+    }
 
 }
